c35a80ce5e8a3c7c27c8d2a91d2a1a3aa233270b
TheFonci <Andrews_am02@hotmail.com>
2023-09-26 17:01:28 -0300

    Fila encadeada.c

---
+++ Prova  terceira unidade/output/departamento.c
@@ -0,0 +1,31 @@
+#include"departamento.h"
+
+void capitalizeString(char *str)
+{
+    if (str[0] != '\0')
+    {
+        str[0] = toupper(str[0]);
+        for (int i = 1; str[i] != '\0'; i++)
+        {
+            str[i] = tolower(str[i]);
+        }
+    }
+}
+
+void preencher(Departamento* departamento){
+
+printf("Nome do departamento");
+scanf(" %[^\n]", departamento->nome);
+capitalizeString(departamento->nome);
+printf("Produto");
+scanf(" %[^\n]", departamento->produto);
+capitalizeString(departamento->produto);
+printf("Porte do produto");
+char porte[10];
+
+while(porte != 'Grande' || porte != 'Medio' || porte != 'Pequeno'){
+    scanf(" %[^\n]", departamento->porte);
+    capitalizeString(departamento->porte);
+}
+  
+}
---
+++ Prova  terceira unidade/output/main.c
@@ -0,0 +1,2 @@
+#include"departamento.c"
+
